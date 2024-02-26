68186ef76e91759eebafea06968cf545ad48f76b
xeij <q08d@aol.com>
2024-02-25 22:16:59 -0800

    2/25/2024

---
+++ art.py
@@ -0,0 +1,13 @@
+import turtle
+import random
+
+turtle.bgcolor('black')
+turtle.colormode(255)
+turtle.speed(0)
+
+for x in range(500): 
+    r,b,g=random.randint(0,255),random.randint(0,255),random.randint(0,255)
+    turtle.pencolor(r,g,b)
+    turtle.fd(x+50)
+    turtle.rt(91)
+    turtle.exitonclick()
